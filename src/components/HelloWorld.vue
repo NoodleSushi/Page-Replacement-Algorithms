bafc87457f94de4f427e6d641aa1a916e38f4ef0
emmantero <emman.tero@yahoo.com.ph>
2023-11-24 22:12:57 +0800

    Page Replacement Algo changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,38 +1,29 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import PageRefAlgoOption from './PageRefAlgoOption.vue'
 
 defineProps<{ msg: string }>()
+
+const PageRefAlgos = ref(["First In First Out (FIFO)", "Least Recently Used (LRU)", "Least Frequently Used (LFU)", "Optimal"])
 
 const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
 
-  <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+  <h1>Page Replacement Algorithms</h1>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+  <PageRefAlgoOption :options="PageRefAlgos" />  
+  
+
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
+  .read-the-docs {
+    color: #888;
+  }
+
+  h1 {
+    position: relative;
+  }
 </style>
---
+++ src/components/PageRefAlgoOption.vue
@@ -0,0 +1,92 @@
+<template>
+        <div class="row">
+            <div class="col">
+                <div class="dropdown-wrapper">
+                    <div class="dropdown-selected-option">
+
+                    </div>
+                </div>
+
+                <div class="PRAlgos-options-wrapper">
+                    <div 
+                        class="options" 
+                        v-for="(option, index) in options" 
+                        :key="index"
+                    >
+                        {{ }}
+                    </div>
+                </div>
+                <!-- <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropdown Button
+                    </button>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">First In First Out (FIFO)</a></li>
+                        <li><a class="dropdown-item" href="#">Least Recently Used (LRU)</a></li>
+                        <li><a class="dropdown-item" href="#">Least Frequently Used (LFU)</a></li>
+                        <li><a class="dropdown-item" href="#">Optimal</a></li>
+                    </ul>
+                </div>   -->
+
+            </div>
+
+            <div class="col">
+                <input type="number" class="form-control" placeholder="Enter num of pages" aria-label="Last name">
+            </div>
+
+            <div class="col">
+                
+                 <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Enter the num of frames
+                    </button>
+                    <ul class="dropdown-menu">
+                        <!-- <li><a class="dropdown-item" href="#">1</a></li>
+                        <li><a class="dropdown-item" href="#">2</a></li> -->
+                        <li><a class="dropdown-item" href="#">3</a></li>
+                        <li><a class="dropdown-item" href="#">4</a></li>
+                        <li><a class="dropdown-item" href="#">5</a></li>
+                        <li><a class="dropdown-item" href="#">6</a></li>
+                        <li><a class="dropdown-item" href="#">7</a></li>
+                    </ul>
+                </div>  
+
+            </div>
+        </div>
+
+       <button type="button" class="generateBtn">Generate</button>
+
+</template>
+
+<style scoped>
+    .dropdown-wrapper {
+        padding: 16px;
+        cursor: pointer; 
+        max-width: 200px; 
+        margin: 0 auto;
+        background-color: gray;
+    }
+
+    .generateBtn{
+        background-color: gold;
+        width: 50%;
+        align-items: center;
+    }
+</style>
+
+<script lang="ts">
+import { defineComponent, defineProps } from 'vue'
+
+const props = defineProps({
+    options: {
+        type: Array,
+        required: true
+    }
+})
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
